bda874729c3aa8f47659b3c34893090b866c9db6
Ilija Trajkoski <ile2807@gmail.com>
2021-09-27 15:03:26 +0200

    terminal test added and filename retrieved

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import cp = require('child_process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,8 +20,15 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hey, I am Jackal Plugin!');
 	});
 	context.subscriptions.push(hello);
-	let tests200  = vscode.commands.registerCommand('jackal-plugin.t200', () => {
-		vscode.window.showInformationMessage('We will now generate Test of HTTP:200 for this collection Jackal Plugin!');
+	let tests200  = vscode.commands.registerCommand('jackal-plugin.t200', (uri:vscode.Uri) => {
+		vscode.window.showInformationMessage(`We will now generate Test of HTTP:200 for this collection ${uri.fsPath}`);
+		cp.exec(`dir`, (err, stdout, stderr) => {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (err) {
+				console.log('error: ' + err);
+			}
+		});
 	});
 	context.subscriptions.push(tests200);
 
