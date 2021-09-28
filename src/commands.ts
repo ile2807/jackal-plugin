76b138e360da7828372cb0374c33d89f1ce02e8d
Ilija Trajkoski <ile2807@gmail.com>
2021-09-28 13:05:21 +0200

    All single file commands added

--- src/commands.ts
+++ src/commands.ts
@@ -1,29 +1,39 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
-import cp = require('child_process');
 import jackal = require('jackal-postman');
 
 export const t200 = vscode.commands.registerCommand('jackal-plugin.t200', (uri: vscode.Uri) => {
-    vscode.window.showInformationMessage(`Alteration started`);
     const executionMessage = jackal.tests200(uri.fsPath, uri.fsPath);
     vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 export const ts = vscode.commands.registerCommand('jackal-plugin.ts', (uri: vscode.Uri) => {
+    const executionMessage = jackal.testsSmart(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const clr = vscode.commands.registerCommand('jackal-plugin.clr', (uri: vscode.Uri) => {
+    const executionMessage = jackal.cleanRequests(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const clv = vscode.commands.registerCommand('jackal-plugin.clv', (uri: vscode.Uri) => {
+    const executionMessage = jackal.cleanVariables(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const arph = vscode.commands.registerCommand('jackal-plugin.arph', (uri: vscode.Uri) => {
+    const executionMessage = jackal.appendRequestHash(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const amcv = vscode.commands.registerCommand('jackal-plugin.amcv', (uri: vscode.Uri) => {
+    const executionMessage = jackal.appendMissingVariables(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const ged = vscode.commands.registerCommand('jackal-plugin.ged', (uri: vscode.Uri) => {
+    const executionMessage = jackal.generateDescriptions(uri.fsPath, uri.fsPath);
+    vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
 });
 
 export const emd = vscode.commands.registerCommand('jackal-plugin.emd', (uri: vscode.Uri) => {
@@ -37,7 +47,8 @@ export const emd = vscode.commands.registerCommand('jackal-plugin.emd', (uri: vs
     };
     vscode.window.showSaveDialog(options).then(fileUri => {
         if (fileUri) {
-            vscode.window.showInformationMessage(`Selected ${fileUri.fsPath}`);
+            const executionMessage = jackal.exportDescriptionMD(uri.fsPath, fileUri.fsPath);
+            vscode.window.showInformationMessage(`File updated with message: ${executionMessage}`);
         }
     });
 });
